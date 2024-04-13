12e4cc89d1f14fac77faac02a18d9b237ea6d975
i-anandsingh <singh.anand974@gmail.com>
2024-04-14 01:18:29 +0530

    Seller Entity updated

--- IdGenerationClient/src/main/java/com/org/entity/SellerRegistrationEntity.java
+++ IdGenerationClient/src/main/java/com/org/entity/SellerRegistrationEntity.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 import java.io.Serializable;
 import java.util.Date;
 
